ec4188f5b2e2be92136aa8df8484a3a7b70ff621
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-04-24 22:47:49 +0700

    Инициализация проекта

---
+++ src/main/java/ru/tsu/hits/hitsinternship/HitsInternshipApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.hitsinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitsInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitsInternshipApplication.class, args);
+    }
+
+}
