659f35ca008caf7f20fd20da1c6b86b7a5be09b7
ZubanovaAlina <alinazubanova5@gmail.com>
2024-04-28 16:51:23 +0700

    Написала методы для удаления и получения групп.

--- src/main/java/ru/tsu/hits/hitsinternship/controller/GroupController.java
+++ src/main/java/ru/tsu/hits/hitsinternship/controller/GroupController.java
@@ -5,13 +5,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.tsu.hits.hitsinternship.dto.group.GroupDto;
 import ru.tsu.hits.hitsinternship.dto.group.NewGroupDto;
 import ru.tsu.hits.hitsinternship.service.GroupService;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/groups")
@@ -26,6 +26,19 @@ public class GroupController {
     GroupDto createGroup(@Valid @RequestBody NewGroupDto newGroupDto) {
 
         return groupService.createGroup(newGroupDto);
+    }
+
+    @Operation(summary = "Удалить группу")
+    @DeleteMapping("/{groupId}")
+    void deleteGroup(@Valid @PathVariable UUID groupId) {
+
+        groupService.deleteGroup(groupId);
+    }
+
+    @Operation(summary = "Получить список групп")
+    @GetMapping("")
+    List<GroupDto> getGroups() {
+        return groupService.getGroups();
     }
 
 }
--- src/main/java/ru/tsu/hits/hitsinternship/service/GroupService.java
+++ src/main/java/ru/tsu/hits/hitsinternship/service/GroupService.java
@@ -1,12 +1,17 @@
 package ru.tsu.hits.hitsinternship.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.tsu.hits.hitsinternship.dto.group.GroupDto;
 import ru.tsu.hits.hitsinternship.dto.group.NewGroupDto;
 import ru.tsu.hits.hitsinternship.entity.GroupEntity;
+import ru.tsu.hits.hitsinternship.exception.NotFoundException;
 import ru.tsu.hits.hitsinternship.mapper.GroupMapper;
 import ru.tsu.hits.hitsinternship.repository.GroupRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,4 +27,21 @@ public class GroupService {
         return groupMapper.entityToDto(group);
     }
 
+    @Transactional
+    public void deleteGroup(UUID groupId) {
+
+        if (!groupRepository.existsById(groupId)) {
+            throw new NotFoundException("Группы с таким id не существует");
+        }
+        groupRepository.deleteById(groupId);
+    }
+
+    public List<GroupDto> getGroups() {
+
+        return groupRepository.findAll().stream()
+                .map(groupMapper::entityToDto)
+                .toList();
+    }
 }
+
+
