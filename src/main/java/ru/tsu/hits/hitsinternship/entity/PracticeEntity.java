96d9a18f0f5b78ad6828e33a4ab6f5b25693459b
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-26 18:36:43 +0700

    Заменил @Data на сущности

--- src/main/java/ru/tsu/hits/hitsinternship/entity/PracticeEntity.java
+++ src/main/java/ru/tsu/hits/hitsinternship/entity/PracticeEntity.java
@@ -1,14 +1,12 @@
 package ru.tsu.hits.hitsinternship.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
