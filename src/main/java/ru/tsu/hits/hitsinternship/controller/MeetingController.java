ce16e58e0e7b2a85f2d47e8754679703e27c5856
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-29 01:36:54 +0700

    Добавил @Tag в MeetingController

--- src/main/java/ru/tsu/hits/hitsinternship/controller/MeetingController.java
+++ src/main/java/ru/tsu/hits/hitsinternship/controller/MeetingController.java
@@ -2,6 +2,7 @@ package ru.tsu.hits.hitsinternship.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Встречи")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/meetings")
