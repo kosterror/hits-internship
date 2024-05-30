f1b646062caf3ed435543434dee43f3bdad70ccf
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-05-31 01:42:50 +0700

    Поправил описание модели для openapi

--- src/main/java/ru/tsu/hits/hitsinternship/dto/practice/PracticeReportDto.java
+++ src/main/java/ru/tsu/hits/hitsinternship/dto/practice/PracticeReportDto.java
@@ -7,21 +7,15 @@ import ru.tsu.hits.hitsinternship.dto.user.UserDto;
 
 import java.util.List;
 
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
-
 @Data
 @AllArgsConstructor
 public class PracticeReportDto {
 
     @Schema(description = "Список мест практик. На уровне БД в рамках одного семестра у одного пользователя не " +
-            "может быть больше одной практики",
-            requiredMode = REQUIRED
-    )
+            "может быть больше одной практики")
     private List<PracticeDto> practices;
 
-    @Schema(description = "Список студентов без мест практик. Не может быть null, но может быть пустым",
-            requiredMode = REQUIRED
-    )
+    @Schema(description = "Список студентов без мест практик")
     private List<UserDto> studentsWithoutPractice;
 
 }
