0b64eba5c8907f41e439f58e48ea24394671430e
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-06-18 22:24:27 +0700

    Добавил проверку на использование специальности перед её удалением

--- src/main/java/ru/tsu/hits/hitsinternship/entity/SpecialityEntity.java
+++ src/main/java/ru/tsu/hits/hitsinternship/entity/SpecialityEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -18,4 +19,10 @@ public class SpecialityEntity {
     private UUID id;
 
     private String name;
+
+    @OneToMany(mappedBy = "speciality")
+    private List<PositionEntity> positions;
+
+    @OneToMany(mappedBy = "speciality")
+    private List<CompanyWishesEntity> companyWishes;
 }
--- src/main/java/ru/tsu/hits/hitsinternship/service/SpecialityService.java
+++ src/main/java/ru/tsu/hits/hitsinternship/service/SpecialityService.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Service;
 import ru.tsu.hits.hitsinternship.dto.specialties.NewSpecialityDto;
 import ru.tsu.hits.hitsinternship.dto.specialties.SpecialityDto;
 import ru.tsu.hits.hitsinternship.entity.SpecialityEntity;
+import ru.tsu.hits.hitsinternship.exception.ConflictException;
 import ru.tsu.hits.hitsinternship.exception.NotFoundException;
 import ru.tsu.hits.hitsinternship.mapper.SpecialityMapper;
 import ru.tsu.hits.hitsinternship.repository.SpecialityRepository;
 
 import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Service
 @RequiredArgsConstructor
@@ -26,19 +29,27 @@ public class SpecialityService {
     }
 
     public void deleteSpeciality(UUID specialityId) {
+        var speciality = findById(specialityId);
 
-        if (!specialityRepository.existsById(specialityId)) {
-            throw new NotFoundException("Специальности с таким id не существует");
+        if (!isEmpty(speciality.getPositions()) || !isEmpty(speciality.getCompanyWishes())) {
+            throw new ConflictException(
+                    "Направление '%s' используется в пожеланиях компаний или позициях студентов"
+                            .formatted(speciality.getName())
+            );
         }
-        specialityRepository.deleteById(specialityId);
 
+        specialityRepository.deleteById(specialityId);
     }
 
     public List<SpecialityDto> getSpecialities() {
-
         return specialityRepository.findAll().stream()
                 .map(specialityMapper::entityToDto)
                 .toList();
+    }
+
+    private SpecialityEntity findById(UUID id) {
+        return specialityRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Speciality %s not found".formatted(id)));
     }
 
 }
