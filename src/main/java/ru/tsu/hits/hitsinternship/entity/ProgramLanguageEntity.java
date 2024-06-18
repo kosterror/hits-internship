f77b847c678c6311df611cbedd72cc6505d5e329
Kosnstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2024-06-18 21:52:11 +0700

    Убрал билдер у ProgramLanguageEntity

--- src/main/java/ru/tsu/hits/hitsinternship/entity/ProgramLanguageEntity.java
+++ src/main/java/ru/tsu/hits/hitsinternship/entity/ProgramLanguageEntity.java
@@ -2,18 +2,14 @@ package ru.tsu.hits.hitsinternship.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
+@Getter
+@Setter
 @Table(name = "program_language")
 public class ProgramLanguageEntity {
 
